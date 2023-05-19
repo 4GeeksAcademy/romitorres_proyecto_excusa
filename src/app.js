9a120eeb176454e19feb7889484f9a3057d6c82b
RomiTorres <110548507+RomiTorres@users.noreply.github.com>
2023-05-19 09:23:49 +0000

    Proyecto corregido

--- src/app.js
+++ src/app.js
@@ -5,19 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
 window.onload = function() {
   //write your code here
 
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
   function aleatorio(who, action, what, when) {
     let whonum = Math.floor(Math.random() * who.length);
     let actionnum = Math.floor(Math.random() * action.length);
@@ -33,10 +33,12 @@ window.onload = function() {
       when[whennum]
     );
   }
-
-  function onLoad() {
-    var excusa = document.getElementById("#excuse");
-    excusa.innerHTML = aleatorio(who, action, what, when);
-  }
+  document.querySelector("#excuse").innerHTML = aleatorio(
+    who,
+    action,
+    what,
+    when
+  );
+  // function onLoad() {}
   console.log("Hello Rigo from the console!");
 };
